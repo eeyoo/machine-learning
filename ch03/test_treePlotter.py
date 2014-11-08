e07eaf6dbd674d07767a7ee12e492b5a753746e1
Feilin Wu <feilinwu2036@gmail.com>
2014-11-08 22:28:37 +0800

    于宿舍更新

--- ch03/test_treePlotter.py
+++ ch03/test_treePlotter.py
@@ -7,5 +7,11 @@ import treePlotter
 ##print treePlotter.retrieveTree(1)
 
 myTree = treePlotter.retrieveTree (0)
-print treePlotter.getNumLeafs(myTree), '\n'
-print treePlotter.getTreeDepth(myTree)
+##print treePlotter.getNumLeafs(myTree), '\n'
+##print treePlotter.getTreeDepth(myTree)
+
+myTree['no surfacing'][3] = 'maybe'
+print myTree
+
+treePlotter.createPlot(myTree)
+
--- ch03/treePlotter.py
+++ ch03/treePlotter.py
@@ -13,13 +13,13 @@ def plotNode(nodeTxt, centerPt, parentPt, nodeType):
     xytext=centerPt, textcoords='axes fraction', \
     va="center", ha="center", bbox=nodeType, arrowprops=arrow_args)
 
-def createPlot():
-    fig = plt.figure(1, facecolor='white')
-    fig.clf()
-    createPlot.ax1 = plt.subplot(111, frameon=False)
-    plotNode(U'决策节点', (0.5, 0.1), (0.3, 0.8), decisionNode)
-    plotNode(U'叶节点', (0.8, 0.1), (0.3, 0.8), leafNode)
-    plt.show()
+##def createPlot():
+##    fig = plt.figure(1, facecolor='white')
+##    fig.clf()
+##    createPlot.ax1 = plt.subplot(111, frameon=False)
+##    plotNode(U'决策节点', (0.5, 0.1), (0.3, 0.8), decisionNode)
+##    plotNode(U'叶节点', (0.8, 0.1), (0.3, 0.8), leafNode)
+##    plt.show()
 
 def getNumLeafs(myTree):
     numLeafs = 0
@@ -49,5 +49,46 @@ def retrieveTree(i):
                      {0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'}}}}
                    ]
     return listOfTrees[i]
+
+def plotMidText(cntrPt, parentPt, txtString):
+    ''' text between parent node and children node '''
+    xMid = (parentPt[0] - cntrPt[0])/2.0 + cntrPt[0]
+    yMid = (parentPt[1] - cntrPt[1])/2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString)
+
+def plotTree(myTree, parentPt, nodeTxt):
+    ''' compute tree's width and depth '''
+    numLeafs = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = myTree.keys()[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs))/2.0/plotTree.totalW, \
+              plotTree.yOff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':
+            plotTree(secondDict[key], cntrPt, str(key))
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), \
+                     cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
+
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5/plotTree.totalW; plotTree.yOff = 1.0;
+    plotTree(inTree, (0.5, 1.0), '')
+    plt.show()
+
+
+            
 
 

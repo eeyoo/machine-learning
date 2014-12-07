f12d0f051b87175d52f95e8d3beaa18bacdbccb2
feilin wu <feilinwu2036@gmail.com>
2014-12-08 00:20:08 +0800

    svm 'split' problem unfixed

--- ch06/test_svm.py
+++ ch06/test_svm.py
@@ -1,5 +1,19 @@
 import svmMLiA
+#from numpy import *
 
-dataArr, labelArr = svmMLiA.loadDataSet('testSet.txt')
-print labelArr
+#dataArr, labelArr = svmMLiA.loadDataSet('testSet.txt')
+#print labelArr, "\n"
 
+#b, alphas = svmMLiA.smoSimple(dataArr, labelArr, 0.6, 0.001, 40)
+#print b
+
+#b, alphas = svmMLiA.smoP(dataArr, labelArr, 0.6, 0.001, 20)
+
+#ws = svmMLiA.calcWs(alphas, dataArr, labelArr)
+#print ws
+
+#svmMLiA.testRbf()
+#svmMLiA.testRbf(2.1)
+
+svmMLiA.testDigits()
+svmMLiA.testDigits(('rbf',20))
